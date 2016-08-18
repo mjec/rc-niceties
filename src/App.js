a589bb5492087b45713b212fa656c19c8fcc2ea7
Taylor Burgess <tayloraburgess@gmail.com>
2016-08-17 22:33:02 -0400

    Add input forms to Person render function

--- src/App.js
+++ src/App.js
@@ -4,71 +4,6 @@ import Remarkable from 'remarkable';
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// - CommentBox
-//  - CommentList
-//   - Comment
-
-// var Comment = React.createClass({
-//     rawMarkup: function() {
-//         var md = new Remarkable();
-//         var rawMarkup = md.render(this.props.children.toString());
-//         return { __html: rawMarkup };
-//     },
-
-//     render: function() {
-//         return (
-//             <div className="comment">
-//               <h2 className="commentAuthor">
-//                 {this.props.author}
-//               </h2>
-//               <span dangerouslySetInnerHTML={this.rawMarkup()} />
-//             </div>
-//         );
-//     }
-// });
-
-// var People = React.createClass({
-//     render: function() {
-//         var Person = this.props.data.map(function(comment) {
-//             var dateEnd = new Date(comment.stints[0].end_date.toString());
-//             var timeDiff = dateEnd.getTime() - Date.now();
-//             var difference = Math.ceil(timeDiff / (1000 * 3600 * 24));
-//             var diffDays = (difference > 0) ? difference : 0;
-//             if (diffDays != 0)
-//                 return (
-//                     <div className="node">
-//                          <img src={comment.avatar_url} className="img-responsive" />
-//                          <Comment author={comment.name} key={comment.id}>
-//                            {diffDays}
-//                          </Comment>
-                         
-//                     </div>
-//                 );
-//         });
-//         return (
-//             <div className="person">
-//               {Person}
-//             </div>
-//         );
-//     }
-// });
-
 var People = React.createClass({
 
     render: function() {
@@ -83,20 +18,46 @@ var People = React.createClass({
                 console.log(dataa);
                return (
             <div className="people">
-              {dataa.map(function(result) {
-                    return <Person key={result.id} data={result}/>;
-                })
-            }
+                {dataa.map(function(result) {
+                        return <Person key={result.id} data={result}/>;
+                    })
+                }
             </div>
         );
     }
 });
 
 var Person = React.createClass({
+    saveCommentsToServer: function() {
+        $.ajax({
+            url: 'api/v1/niceties/28/' + this.props.data.id,
+            dataType: 'json',
+            type: 'POST',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    getInitialState: function() {
+        return {value: 'Hello!'};
+    },
+
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+
     render: function() {
         return ( 
             <div className="person">
                 <img src={this.props.data.avatar_url} className="img-responsive" />
+                <form>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </form>
             </div>
         );
     }
