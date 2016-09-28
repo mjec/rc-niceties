87427ef1362eba89bd45f3496879ffd4c583de02
kr <kangren.chia@gmail.com>
2016-09-28 09:03:04 +0800

    cleaned up api.py, separated caching calls from api endpoints that return json data, and renamed endpoints to be more descriptive

--- backend/api.py
+++ backend/api.py
@@ -81,8 +81,6 @@ def cache_people_call(batch_id):
         cache.set(cache_key, people)
     return people
 
-@app.route('/api/v1/people/<int:person_id>')
-@needs_authorization
 def cache_person_call(person_id):
     cache_key = 'person:{}'.format(person_id)
     try:
@@ -91,7 +89,7 @@ def cache_person_call(person_id):
         p = rc.get('people/{}'.format(person_id)).data
         if 'message' in p:
             return redirect(url_for('login'))
-        person = {
+        person_info = {
             'id': p['id'],
             'name': p['first_name'],
             'full_name': p['first_name'] + " " + p['last_name'],
@@ -105,162 +103,8 @@ def cache_person_call(person_id):
             'twitter': p['twitter'],
             'github': p['github'],
         }
-        person_json = jsonify(person)
-        cache.set(cache_key, person_json)
-    return person_json
-
-# @app.route('api/v1/window')
-# @needs_authorization
-# def get_window_info():
-#     ret = [batch for batch in batches if util.open_batches(batch['end_date'])]
-#     if not util.niceties_are_open(batches) or len(ret) == 1:
-#         ret = []
-#     return jsonify(
-#         {
-#             "status": 'closed',
-#             "opening": '',
-#         })
-
-@app.route('/api/v1/self')
-@needs_authorization
-def get_self_info():
-    print(rc.access_token_url)
-    self_info = rc.get('people/me').data
-    if 'message' in self_info:
-        return redirect(url_for('login'))
-    return jsonify(self_info)
-
-@app.route('/api/v1/all-niceties', methods=['GET'])
-@needs_authorization
-def all_niceties():
-    ret = {}    # Mapping from target_id to a list of niceties for that person
-    last_target = None
-    is_rachel = current_user().id == 770
-    two_weeks_from_now = datetime.now() - timedelta(days=14)
-    if is_rachel == True:
-        valid_niceties = (Nicety.query
-                          #.filter(Nicety.end_date < two_weeks_from_now)
-                          .order_by(Nicety.target_id)
-                          .all())
-        for n in valid_niceties:
-            if n.target_id != last_target:
-                # ... set up the test for the next one
-                last_target = n.target_id
-                ret[n.target_id] = []  # initialize the dictionary
-            if n.anonymous == False:
-                ret[n.target_id].append({
-                    'author_id': n.author_id,
-                    'name': json.loads(cache_person_call(n.author_id).data)['full_name'],
-                    'no_read': n.no_read,
-                    'text': n.text,
-                })
-            else:
-                ret[n.target_id].append({
-                    'text': n.text,
-                    'no_read': n.no_read,
-                })
-        return jsonify([
-            {
-                'to_name': json.loads(cache_person_call(k).data)['full_name'],
-                'to_id': json.loads(cache_person_call(k).data)['id'],
-                'niceties': v
-            }
-            for k, v in ret.items()
-        ])
-    else:
-        return jsonify({'authorized': "false"})
-
-@app.route('/api/v1/all-niceties', methods=['POST'])
-@needs_authorization
-def overwrite_niceties():
-    is_rachel = current_user().id == 770
-    nicety_text = request.form.get("text")
-    nicety_author = request.form.get("author_id")
-    nicety_target = request.form.get("target_id")
-    if is_rachel == True:
-        (Nicety.query
-         .filter(Nicety.author_id==nicety_author)
-         .filter(Nicety.target_id==nicety_target)
-         .update({'text': nicety_text}))
-        db.session.commit()
-        return jsonify({'status': 'OK'})
-    else:
-        return jsonify({'authorized': "false"})
-
-@app.route('/api/v1/load-niceties')
-@needs_authorization
-def load_unsent_niceties():
-    user_id = current_user().id
-    niceties = (Nicety.query
-                .filter(Nicety.author_id == user_id)
-                .filter(Nicety.end_date > datetime.now())
-                .all())
-    ret = [{
-        'target_id': n.target_id,
-        'text': n.text,
-        'anonymous': n.anonymous,
-        'no_read': n.no_read,
-        'date_updated': n.date_updated
-    } for n in niceties]
-    return jsonify(ret)
-
-@app.route('/api/v1/niceties-from-me')
-@needs_authorization
-def niceties_from_me():
-    user_id = current_user().id
-    niceties = (Nicety.query
-                .filter(Nicety.author_id == user_id)
-                .all())
-    ret = [{
-        'target_id': n.target_id,
-        'text': n.text,
-        'anonymous': n.anonymous,
-        'no_read': n.no_read,
-        'date_updated': n.date_updated
-    } for n in niceties]
-    return jsonify(ret)
-
-@app.route('/api/v1/show-niceties')
-@needs_authorization
-def get_niceties_for_current_user():
-    ret = []
-    whoami = current_user().id
-    two_weeks_from_now = datetime.now() - timedelta(days=14)
-    valid_niceties = (Nicety.query
-                      .filter(Nicety.end_date + timedelta(days=1) < datetime.now()) # show niceties one day after the end date
-                      .filter(Nicety.target_id == whoami)
-                      .all())
-    for n in valid_niceties:
-        if n.text != None:
-            if n.anonymous == True:
-                store = {
-                    'end_date': n.end_date,
-                    'anonymous': n.anonymous,
-                    'text': n.text,
-                    'no_read': n.no_read,
-                    'date_updated': n.date_updated
-                }
-            else:
-                store = {
-                    'avatar_url': json.loads(cache_person_call(n.author_id).data)['avatar_url'],
-                    'name': json.loads(cache_person_call(n.author_id).data)['name'],
-                    'author_id': n.author_id,
-                    'end_date': n.end_date,
-                    'anonymous': n.anonymous,
-                    'text': n.text,
-                    'no_read': n.no_read,
-                    'date_updated': n.date_updated
-                }
-            ret.append(store)
-    return jsonify(ret)
-    pass
-
-
-@app.route('/api/v1/faculty')
-@needs_authorization
-def get_faculty():
-    faculty = get_current_faculty()
-    return jsonify(faculty)
+        cache.set(cache_key, person_info)
+    return person_info
 
 def get_current_faculty():
     ''' faculty will always appear in
@@ -272,11 +116,6 @@ def get_current_faculty():
             if p['is_faculty'] == True:
                 faculty.append(p)
     return faculty
-
-@app.route('/api/v1/batches')
-def get_all_batches():
-    batches = cache_batches_call()
-    return jsonify(batches)
 
 def get_current_batches_info():
     ret = [batch for batch in cache_batches_call() if util.open_batches(batch['end_date'])]
@@ -319,6 +158,153 @@ def partition_current_users(users):
                 pass
     return ret
 
+@app.route('/api/v1/people/<int:person_id>')
+@needs_authorization
+def get_person_info(person_id):
+    person_info = cache_person_call(person_id)
+    return jsonify(person_info)
+
+
+# @app.route('api/v1/window')
+# @needs_authorization
+# def get_window_info():
+#     ret = [batch for batch in batches if util.open_batches(batch['end_date'])]
+#     if not util.niceties_are_open(batches) or len(ret) == 1:
+#         ret = []
+#     return jsonify(
+#         {
+#             "status": 'closed',
+#             "opening": '',
+#         })
+
+@app.route('/api/v1/self')
+@needs_authorization
+def get_self_info():
+    print(rc.access_token_url)
+    self_info = rc.get('people/me').data
+    if 'message' in self_info:
+        return redirect(url_for('login'))
+    return jsonify(self_info)
+
+@app.route('/api/v1/admin-edit-niceties', methods=['GET'])
+@needs_authorization
+def post_edited_niceties():
+    ret = {}    # Mapping from target_id to a list of niceties for that person
+    last_target = None
+    is_rachel = current_user().id == 770
+    two_weeks_from_now = datetime.now() - timedelta(days=14)
+    if is_rachel == True:
+        valid_niceties = (Nicety.query
+                          #.filter(Nicety.end_date < two_weeks_from_now)
+                          .order_by(Nicety.target_id)
+                          .all())
+        for n in valid_niceties:
+            if n.target_id != last_target:
+                # ... set up the test for the next one
+                last_target = n.target_id
+                ret[n.target_id] = []  # initialize the dictionary
+            if n.anonymous == False:
+                ret[n.target_id].append({
+                    'author_id': n.author_id,
+                    'name': json.loads(cache_person_call(n.author_id).data)['full_name'],
+                    'no_read': n.no_read,
+                    'text': n.text,
+                })
+            else:
+                ret[n.target_id].append({
+                    'text': n.text,
+                    'no_read': n.no_read,
+                })
+        return jsonify([
+            {
+                'to_name': json.loads(cache_person_call(k).data)['full_name'],
+                'to_id': json.loads(cache_person_call(k).data)['id'],
+                'niceties': v
+            }
+            for k, v in ret.items()
+        ])
+    else:
+        return jsonify({'authorized': "false"})
+
+@app.route('/api/v1/admin-edit-niceties', methods=['POST'])
+@needs_authorization
+def get_niceties_to_edit():
+    is_rachel = current_user().id == 770
+    nicety_text = request.form.get("text")
+    nicety_author = request.form.get("author_id")
+    nicety_target = request.form.get("target_id")
+    if is_rachel == True:
+        (Nicety.query
+         .filter(Nicety.author_id==nicety_author)
+         .filter(Nicety.target_id==nicety_target)
+         .update({'text': nicety_text}))
+        db.session.commit()
+        return jsonify({'status': 'OK'})
+    else:
+        return jsonify({'authorized': "false"})
+
+@app.route('/api/v1/niceties-from-me')
+@needs_authorization
+def niceties_from_me():
+    user_id = current_user().id
+    niceties = (Nicety.query
+                .filter(Nicety.author_id == user_id)
+                .all())
+    ret = [{
+        'target_id': n.target_id,
+        'text': n.text,
+        'anonymous': n.anonymous,
+        'no_read': n.no_read,
+        'date_updated': n.date_updated
+    } for n in niceties]
+    return jsonify(ret)
+
+@app.route('/api/v1/niceties-for-me')
+@needs_authorization
+def niceties_for_me():
+    ret = []
+    whoami = current_user().id
+    two_weeks_from_now = datetime.now() - timedelta(days=14)
+    valid_niceties = (Nicety.query
+                      .filter(Nicety.end_date + timedelta(days=1) < datetime.now()) # show niceties one day after the end date
+                      .filter(Nicety.target_id == whoami)
+                      .all())
+    for n in valid_niceties:
+        if n.text != None:
+            if n.anonymous == True:
+                store = {
+                    'end_date': n.end_date,
+                    'anonymous': n.anonymous,
+                    'text': n.text,
+                    'no_read': n.no_read,
+                    'date_updated': n.date_updated
+                }
+            else:
+                store = {
+                    'avatar_url': json.loads(cache_person_call(n.author_id).data)['avatar_url'],
+                    'name': json.loads(cache_person_call(n.author_id).data)['name'],
+                    'author_id': n.author_id,
+                    'end_date': n.end_date,
+                    'anonymous': n.anonymous,
+                    'text': n.text,
+                    'no_read': n.no_read,
+                    'date_updated': n.date_updated
+                }
+            ret.append(store)
+    return jsonify(ret)
+    pass
+
+@app.route('/api/v1/faculty')
+@needs_authorization
+def get_faculty():
+    faculty = get_current_faculty()
+    return jsonify(faculty)
+
+@app.route('/api/v1/batches')
+def get_all_batches():
+    batches = cache_batches_call()
+    return jsonify(batches)
+
 @app.route('/api/v1/people')
 @needs_authorization
 def display_people():
@@ -337,7 +323,7 @@ def display_people():
             leaving.append(person)
     staying = list(person for person in people['staying'])
     faculty = get_current_faculty()
-    # there needs to be a better way to do this!
+    # there needs to be a better way to add special people to the current exiting batch
     special = []
     random.seed(current_user().random_seed)
     random.shuffle(staying)
@@ -356,7 +342,7 @@ def display_people():
         }
     return jsonify(to_display)
 
-@app.route('/api/v1/post-niceties', methods=['POST'])
+@app.route('/api/v1/save-niceties', methods=['POST'])
 @needs_authorization
 def save_niceties():
     niceties_to_save = json.loads(request.form.get("niceties", "[]"))
--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import './index.css';
 
 ReactDOM.render(
     <App people_api="/api/v1/people"
-         post_nicety_api="/api/v1/post-niceties"
-         get_nicety_api="/api/v1/show-niceties"
-         all_niceties_api="/api/v1/all-niceties"
+         post_nicety_api="/api/v1/save-niceties"
+         get_nicety_api="/api/v1/niceties-for-me"
+         load_nicety_api="/api/v1/niceties-from-me"
+         all_niceties_api="/api/v1/admin-edit-niceties"
          print_nicety_api="/api/v1/niceties-to-print"
-         load_nicety_api="/api/v1/load-niceties"
          self_api="/api/v1/self"
          pollInterval={2000} />,
     document.getElementById('root')
