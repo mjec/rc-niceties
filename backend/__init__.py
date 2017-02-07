b0ae7f370e93c5817b9039514ac183218dbe3b2d
Taylor Burgess <tayloraburgess@gmail.com>
2017-02-06 20:08:34 -0500

    Add OAuth token refreshing

--- backend/__init__.py
+++ backend/__init__.py
@@ -24,19 +24,16 @@ app.static_folder = app.config.get('STATIC_BASE', './static/')
 
 with app.app_context():
     db = SQLAlchemy(app)
-    if MOCK_OUT_RC_API:
-        from backend.mock import rc
-        rc = rc.MockRCOAuthAPI()
-    else:
-        rc = OAuth(app).remote_app(
-            'recurse_center',
-            base_url='https://www.recurse.com/api/v1/',
-            access_token_url='https://www.recurse.com/oauth/token',
-            authorize_url='https://www.recurse.com/oauth/authorize',
-            consumer_key=os.environ['RC_OAUTH_ID'],  # Deliberately throw exception if not set
-            consumer_secret=os.environ['RC_OAUTH_SECRET'],  # Deliberately throw exception it not set
-            access_token_method='POST',
-        )
+    rc = OAuth(app).remote_app(
+        'recurse_center',
+        base_url='https://www.recurse.com/api/v1/',
+        access_token_url='https://www.recurse.com/oauth/token',
+        request_token_url=None,
+        authorize_url='https://www.recurse.com/oauth/authorize',
+        consumer_key=os.environ['RC_OAUTH_ID'],  # Deliberately throw exception if not set
+        consumer_secret=os.environ['RC_OAUTH_SECRET'],  # Deliberately throw exception it not set
+        access_token_method='POST',
+    )
 
 # Imports for URLs that should be available
 import backend.api
