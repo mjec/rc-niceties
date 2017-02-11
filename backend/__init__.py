add503541bb21bd9a4619243e597eb0dbb0279e4
Jason Owen <jason.a.owen@gmail.com>
2017-02-11 18:31:52 -0500

    Fix comment on static routes
    Looks like a search & replace went awry. Also reformat that comment to
    have shorter lines, to make pep8 happier.

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,8 +6,8 @@ from flask_oauthlib.client import OAuth
 
 MOCK_OUT_RC_API = False
 
-# Flask won't rou    RLs in the static_url_path, so we set it to something arbitrary
-# and unlikely to    ever used (hsbackendence the included random GUID).
+# Flask won't route URLs in the static_url_path, so we set it to something
+# arbitrary and unlikely to be ever used (hence the included random GUID).
 app = Flask(__name__, static_url_path='/noroute/aeae9ce2-1457-494f-9881-29d9df71a526')
 app.config.from_object(__name__)
 
