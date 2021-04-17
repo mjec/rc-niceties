9db201f81958ddc60daa482bf6d3e07d13734a90
Jason Owen <jason@jasonaowen.net>
2021-04-17 01:24:50 -0700

    Move row calculation out of <NicetyDisplay>
    The function that splits a list of niceties into chunks of 4 is not
    specific to <NicetyDisplay>; in fact, it can (and probably should) be
    replaced with lodash's chunk[1] function. However, for now, simply move
    it out of the class and into a standalone function, as a preparatory
    step towards converting <NicetyDisplay> into a function component.
    
    [1] https://lodash.com/docs/4.17.15#chunk

--- src/components/NicetyDisplay.js
+++ src/components/NicetyDisplay.js
@@ -3,26 +3,26 @@ import { Grid } from 'react-bootstrap';
 
 import NicetyRow from './NicetyRow';
 
-const NicetyDisplay = React.createClass({
-    generateRows: function() {
-        let dataList = [];
-        for (let i = 0; i < this.props.niceties.length; i +=4) {
-            let row = [];
-            for (let j = 0; j < 4; j++) {
-                if ((i + j) < this.props.niceties.length) {
-                    row.push(this.props.niceties[i + j]);
-                }
+const generateRows = (input) => {
+    let dataList = [];
+    for (let i = 0; i < input.length; i +=4) {
+        let row = [];
+        for (let j = 0; j < 4; j++) {
+            if ((i + j) < input.length) {
+                row.push(input[i + j]);
             }
-            dataList.push(row);
         }
-        return dataList;
-    },
+        dataList.push(row);
+    }
+    return dataList;
+};
 
+const NicetyDisplay = React.createClass({
     render: function() {
         return (
             <div className="niceties">
                 <Grid>
-                    {this.generateRows().map((row) => <NicetyRow data={row}/>)}
+                    {generateRows(this.props.niceties).map((row) => <NicetyRow data={row}/>)}
                 </Grid>
             </div>
         );}
