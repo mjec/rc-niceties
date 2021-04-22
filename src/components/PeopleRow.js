2ad1eca4944a699b4db24c107a9522e65fcb45ed
Jason Owen <jason@jasonaowen.net>
2021-04-22 00:56:59 -0700

    Convert <PeopleRow> to function component
    The `React.createClass` method of creating React components was
    deprecated with React 15.5.0[1]. We will need to convert each component
    away from using createClass. There were a few alternatives at the time,
    including the recommendation they gave then of `class Example extends
    React.Component`; however, the current preferred trend is function
    components.
    
    Function components were introduced with React 0.14[2], and work great
    when the entirety of a component is its render method. (Later versions
    of React support function components with state and side effects via
    hooks[3], but we're not there yet!)
    
    <PeopleRow> is one of our simpler components, which makes it an ideal
    component for this kind of conversion: it has no state, and no behavior.
    Convert it to be a function component. Due to its simplicity, it can be
    written as an arrow function expression[4].
    
    [1] https://reactjs.org/blog/2017/04/07/react-v15.5.0.html#migrating-from-reactcreateclass
    [2] https://reactjs.org/blog/2015/10/07/react-v0.14.html#stateless-function-components
    [3] https://reactjs.org/blog/2019/02/06/react-v16.8.0.html
    [4] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
    
    Issue #18 Upgrade React

--- src/components/PeopleRow.js
+++ src/components/PeopleRow.js
@@ -3,24 +3,20 @@ import { Row, Col } from 'react-bootstrap';
 
 import Person from "./Person";
 
-const PeopleRow = React.createClass({
-    render: function() {
-        return (
-            <Row>
-                {this.props.data.map((result) => (
-                    <Col lg="3" md="6" sm="6" xs="12">
-                        <Person
-                            fromMe={this.props.fromMe}
-                            data={result}
-                            saveReady={this.props.saveReady}
-                            saveButton={this.props.saveButton}
-                            updated_niceties={this.props.updated_niceties}
-                        />
-                    </Col>
-                ))}
-            </Row>
-        );
-    }
-});
+const PeopleRow = (props) => (
+    <Row>
+        {props.data.map((result) => (
+            <Col lg="3" md="6" sm="6" xs="12">
+                <Person
+                    fromMe={props.fromMe}
+                    data={result}
+                    saveReady={props.saveReady}
+                    saveButton={props.saveButton}
+                    updated_niceties={props.updated_niceties}
+                />
+            </Col>
+        ))}
+    </Row>
+);
 
 export default PeopleRow;
