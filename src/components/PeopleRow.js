454e51940211e0dc4bef6e249bbed2723a32ea32
Jason Owen <jason@jasonaowen.net>
2021-04-22 00:51:43 -0700

    Refactor to use arrow function in <PeopleRow> map
    Arrow function expressions[1] are terser and, I think, more readable.
    Replace the function expression in a call to map[2] with an arrow
    function expression, and reformat.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
    [2] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map

--- src/components/PeopleRow.js
+++ src/components/PeopleRow.js
@@ -8,12 +8,17 @@ const PeopleRow = React.createClass({
         const saveButton = this.props.saveButton;
         return (
             <Row>
-              {this.props.data
-                .map(function(result) {
-                  return (<Col lg="3" md="6" sm="6" xs="12">
-                    <Person fromMe={this.props.fromMe} data={result} saveReady={this.props.saveReady} saveButton={saveButton} updated_niceties={this.props.updated_niceties}/>
-                  </Col>);
-                }.bind(this))}
+                {this.props.data.map((result) => (
+                    <Col lg="3" md="6" sm="6" xs="12">
+                        <Person
+                            fromMe={this.props.fromMe}
+                            data={result}
+                            saveReady={this.props.saveReady}
+                            saveButton={saveButton}
+                            updated_niceties={this.props.updated_niceties}
+                        />
+                    </Col>
+                ))}
             </Row>
         );
     }
