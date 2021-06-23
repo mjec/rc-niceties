07fac2c2c2143a9917ff47f5808f28b7e949ff2b
Christalee Bieber <cbieber@alum.mit.edu>
2021-06-23 17:40:31 -0400

    updated People to use ES6 classes instead of React.createClass
    React.createClass is deprecated in React 15.5.0. People has state, so it
    isn't eligible to become a functional component. In the meantime,
    convert it to use regular ES6 classes, following [this
    post](https://daveceddia.com/convert-createclass-to-es6-class/)

--- src/components/People.js
+++ src/components/People.js
@@ -9,169 +9,152 @@ import SaveButton from "./SaveButton";
 
 export let updated_niceties_spinlock = false;
 
-const People = React.createClass({
-    saveAllComments: function() {
-        updated_niceties_spinlock = true;
-        let data_to_save = [];
-        const dateUpdated = new Date(Date.now());
-        const dateUpdatedStr = dateUpdated.toUTCString();
-        this.state.updated_niceties.forEach(function(e) {
-            const split_e = e.split(",");
-            let end_date;
-            if (split_e[1] === "null") {
-              end_date = null;
-            } else {
-              end_date = split_e[1];
-            }
-            const anonymous = store.get("anonymous-" + split_e[0], false);
-            const text = store.get("nicety-" + split_e[0], '');
-            const noRead = store.get("no_read-" + split_e[0], false);
+class People extends React.Component {
+  state = {
+    data: [],
+    noSave: store.get("saved"),
+    justSaved: false,
+    updated_niceties: new Set()
+  }
 
-            data_to_save.push(
-                {
-                    target_id: parseInt(split_e[0], 10),
-                    end_date: end_date,
-                    anonymous: anonymous,
-                    text: text,
-                    no_read: noRead,
-                    date_updated: dateUpdatedStr
-                }
-            );
-        });
-        updated_niceties_spinlock = false;
-        $.ajax({
-            url: this.props.save_nicety_api,
-            data: {'niceties': JSON.stringify(data_to_save)},
-            dataType: 'json',
-            type: 'POST',
-            cache: false,
-            success: function() {
-                this.setState({noSave: true});
-                this.setState({justSaved: true});
-                store.set("saved", true);
-                this.state.updated_niceties.forEach(function(e) {
-                    const split_e = e.split(",");
-                    store.set("date_updated-" + split_e[0], dateUpdatedStr);
-                });
-                this.state.updated_niceties.clear();
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.log(err)
-            }
-        });
-    },
+  saveAllComments = () => {
+      updated_niceties_spinlock = true;
+      let data_to_save = [];
+      const dateUpdated = new Date(Date.now());
+      const dateUpdatedStr = dateUpdated.toUTCString();
+      this.state.updated_niceties.forEach(function(e) {
+          const split_e = e.split(",");
+          let end_date;
+          if (split_e[1] === "null") {
+            end_date = null;
+          } else {
+            end_date = split_e[1];
+          }
+          const anonymous = store.get("anonymous-" + split_e[0], false);
+          const text = store.get("nicety-" + split_e[0], '');
+          const noRead = store.get("no_read-" + split_e[0], false);
 
-    getInitialState: function() {
-        if (store.get("saved") === true) {
-            return {
-                data: [],
-                noSave: true,
-                justSaved: false,
-                updated_niceties: new Set()
-            }
-        } else if (store.get("saved") === false) {
-            return {
-                data: [],
-                noSave: false,
-                justSaved: false,
-                updated_niceties: new Set()
-            }
-        }
-    },
+          data_to_save.push(
+              {
+                  target_id: parseInt(split_e[0], 10),
+                  end_date: end_date,
+                  anonymous: anonymous,
+                  text: text,
+                  no_read: noRead,
+                  date_updated: dateUpdatedStr
+              }
+          );
+      });
+      updated_niceties_spinlock = false;
+      $.ajax({
+          url: this.props.save_nicety_api,
+          data: {'niceties': JSON.stringify(data_to_save)},
+          dataType: 'json',
+          type: 'POST',
+          cache: false,
+          success: function() {
+              this.setState({noSave: true});
+              this.setState({justSaved: true});
+              store.set("saved", true);
+              this.state.updated_niceties.forEach(function(e) {
+                  const split_e = e.split(",");
+                  store.set("date_updated-" + split_e[0], dateUpdatedStr);
+              });
+              this.state.updated_niceties.clear();
+          }.bind(this),
+          error: function(xhr, status, err) {
+              console.log(err)
+          }
+      });
+  }
 
-    generateRows: function(inputArray) {
-        let dataList = [];
-        if (inputArray !== undefined) {
-            for (let i = 0; i < inputArray.length; i +=4) {
-                let row = [];
-                for (let j = 0; j < 4; j++) {
-                    if ((i + j) < inputArray.length) {
-                        row.push(inputArray[i + j]);
-                    }
-                }
-                dataList.push(row);
-            }
-        }
-        return dataList;
-    },
+  generateRows = (inputArray) => {
+      let dataList = [];
+      if (inputArray !== undefined) {
+          for (let i = 0; i < inputArray.length; i +=4) {
+              let row = [];
+              for (let j = 0; j < 4; j++) {
+                  if ((i + j) < inputArray.length) {
+                      row.push(inputArray[i + j]);
+                  }
+              }
+              dataList.push(row);
+          }
+      }
+      return dataList;
+  }
 
-    saveReady: function () {
-        this.setState({noSave: false});
-    },
+  saveReady = () => {
+      this.setState({noSave: false});
+  }
 
-    alertTimer: function() {
-        setTimeout(function () {
-            this.setState({justSaved: false});
-        }.bind(this), 3000);
-    },
+  alertTimer = () => {
+      setTimeout(function () {
+          this.setState({justSaved: false});
+      }.bind(this), 3000);
+  }
 
-    render: function() {
-        let leaving = this.generateRows(this.props.people.leaving);
-        let staying = this.generateRows(this.props.people.staying);
-        let faculty = this.generateRows(this.props.people.faculty);
-        const savePass = this.saveReady.bind(this);
+  render() {
+      let leaving = this.generateRows(this.props.people.leaving);
+      let staying = this.generateRows(this.props.people.staying);
+      let faculty = this.generateRows(this.props.people.faculty);
 
-        let maybeHeader  = '';
-        let maybeHR = '';
-        if (staying.length > 0) {
-            maybeHeader = (<h3>In-Batch</h3>);
-            maybeHR = (<hr />);
-        }
-        if (this.state.justSaved) {
-            this.alertTimer();
-        }
+      let maybeHeader  = '';
+      let maybeHR = '';
+      if (staying.length > 0) {
+          maybeHeader = (<h3>In-Batch</h3>);
+          maybeHR = (<hr />);
+      }
+      if (this.state.justSaved) {
+          this.alertTimer();
+      }
 
-        return (
-            <div className="people">
-              <Modal show={this.state.justSaved}>
-                <Modal.Body>
-                  Niceties Saved!
-                </Modal.Body>
-              </Modal>
-              <div className="save_button">
-                <SaveButton
-                  noSave={this.state.noSave}
-                  onClick={this.saveAllComments}>
-                  Save
-                </SaveButton>
-              </div>
-              <Grid>
-                <hr />
-                <h3>Leaving Soon</h3>
-                {leaving.map(function(row) {
-                  return (
-                    <PeopleRow fromMe={this.props.fromMe} data={row} saveReady={savePass} updated_niceties={this.state.updated_niceties}/>
-                  );
-                }.bind(this))}
-                <hr />
-                { maybeHeader }
-                {staying.map(function(row) {
-                  return (
-                    <PeopleRow fromMe={this.props.fromMe} data={row} saveReady={savePass} updated_niceties={this.state.updated_niceties}/>
-                  );
-                }.bind(this))}
-                { maybeHR }
+      const staffRows = faculty.map((row) => (
+          <PeopleRow fromMe={this.props.fromMe} data={row} saveReady={this.saveReady} updated_niceties={this.state.updated_niceties}/>
+        ))
 
-                <h3>Staff</h3>
-                {faculty.map((row) => (
-                    <PeopleRow
-                        fromMe={this.props.fromMe}
-                        data={row}
-                        saveReady={savePass}
-                        updated_niceties={this.state.updated_niceties}
-                    />
+      return (
+          <div className="people">
+            <Modal show={this.state.justSaved}>
+              <Modal.Body>
+                Niceties Saved!
+              </Modal.Body>
+            </Modal>
+            <div className="save_button">
+              <SaveButton
+                noSave={this.state.noSave}
+                onClick={this.saveAllComments}>
+                Save
+              </SaveButton>
+            </div>
+            <Grid>
+              <hr />
+              <h3>Leaving Soon</h3>
+              {leaving.map((row) => (
+                  <PeopleRow fromMe={this.props.fromMe} data={row} saveReady={this.saveReady} updated_niceties={this.state.updated_niceties}/>
                 ))}
 
-              </Grid>
-              <div className="save_button">
-                <SaveButton
-                  noSave={this.state.noSave}
-                  onClick={this.saveAllComments}>
-                  Save
-                </SaveButton>
-              </div>
+              <hr />
+              { maybeHeader }
+              {staying.map((row) => (
+                  <PeopleRow fromMe={this.props.fromMe} data={row} saveReady={this.saveReady} updated_niceties={this.state.updated_niceties}/>
+                ))}
+
+              { maybeHR }
+              <h3>Staff</h3>
+              { staffRows }
+            </Grid>
+            <div className="save_button">
+              <SaveButton
+                noSave={this.state.noSave}
+                onClick={this.saveAllComments}>
+                Save
+              </SaveButton>
             </div>
-        );}
-});
+          </div>
+      );
+    }
+
+}
 
 export default People;
