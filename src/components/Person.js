eccb17f043889305ad42b334d08368e4f50714a5
Christalee <cbieber@alum.mit.edu>
2021-09-26 09:45:01 -0400

    convert Person to ES6 (#81)
    * refactored Person.js to extend React.Component instead of React.createClass()
    
    React.createClass is deprecated in React 15.5.0. Person has state, so it isn't eligible to become a functional component. In the meantime, convert it to use regular ES6 classes.
    
    * whitespace consistency: 4 spaces

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Checkbox, Image } from 'react-bootstrap';
+import {Checkbox, Image} from 'react-bootstrap';
 import store from 'store2';
 
-import { updated_niceties_spinlock } from "./People";
+import {updated_niceties_spinlock} from "./People";
 
-
-const Person = React.createClass({
-    getInitialState: function() {
+class Person extends React.Component {
+    constructor(props) {
+        super(props)
         let textValue = '';
         let anonValue = false;
         let noReadValue = false;
         let dataPerson;
         let foundPerson = false;
-        for (var i = 0; i < this.props.fromMe.length; i++) {
-            if (this.props.fromMe[i].target_id === this.props.data.id) {
-                dataPerson = this.props.fromMe[i];
+        for (var i = 0; i < props.fromMe.length; i++) {
+            if (props.fromMe[i].target_id === props.data.id) {
+                dataPerson = props.fromMe[i];
                 foundPerson = true;
                 break;
             }
@@ -27,48 +27,50 @@ const Person = React.createClass({
                 dateUpdated = dataPerson.date_updated;
             }
         }
-        if (foundPerson && store.get("date_updated-" + this.props.data.id) === null) {
+        if (foundPerson && store.get("date_updated-" + props.data.id) === null) {
             if (dataPerson.text !== '' && dataPerson.text !== null) {
-                store.set("nicety-" + this.props.data.id, dataPerson.text);
+                store.set("nicety-" + props.data.id, dataPerson.text);
                 textValue = dataPerson.text;
             } else {
                 textValue = '';
             }
-            store.set("anonymous-" + this.props.data.id, dataPerson.anonymous);
+            store.set("anonymous-" + props.data.id, dataPerson.anonymous);
             anonValue = dataPerson.anonymous;
-            store.set("no_read-" + this.props.data.id, dataPerson.no_read);
+            store.set("no_read-" + props.data.id, dataPerson.no_read);
             noReadValue = dataPerson.no_read;
-            store.set("date_updated-" + this.props.data.id, dateUpdated);
-        } else if (foundPerson && store.get("date_updated-" + this.props.data.id) !== dateUpdated) {
+            store.set("date_updated-" + props.data.id, dateUpdated);
+        } else if (foundPerson && store.get("date_updated-" + props.data.id) !== dateUpdated) {
             if (dataPerson.text !== '' && dataPerson.text !== null) {
-                store.set("nicety-" + this.props.data.id, dataPerson.text);
+                store.set("nicety-" + props.data.id, dataPerson.text);
                 textValue = dataPerson.text;
             } else {
                 textValue = '';
             }
-            store.set("anonymous-" + this.props.data.id, dataPerson.anonymous);
+            store.set("anonymous-" + props.data.id, dataPerson.anonymous);
             anonValue = dataPerson.anonymous;
-            store.set("no_read-" + this.props.data.id, dataPerson.no_read);
+            store.set("no_read-" + props.data.id, dataPerson.no_read);
             noReadValue = dataPerson.no_read;
-            store.set("date_updated-" + this.props.data.id, dateUpdated);
+            store.set("date_updated-" + props.data.id, dateUpdated);
         } else {
-            if (store.get("nicety-" + this.props.data.id) !== null) {
-                textValue = store.get("nicety-" + this.props.data.id);
+            if (store.get("nicety-" + props.data.id) !== null) {
+                textValue = store.get("nicety-" + props.data.id);
             }
-            if (store.get("anonymous-" + this.props.data.id) !== null) {
-                anonValue = store.get("anonymous-" + this.props.data.id);
+            if (store.get("anonymous-" + props.data.id) !== null) {
+                anonValue = store.get("anonymous-" + props.data.id);
             }
-            if (store.get("no_read-" + this.props.data.id) !== null) {
-                noReadValue = store.get("no_read-" + this.props.data.id);
+            if (store.get("no_read-" + props.data.id) !== null) {
+                noReadValue = store.get("no_read-" + props.data.id);
             }
         }
-        return {
+
+        this.state = {
             textValue: textValue,
             anonValue: anonValue,
             noReadValue: noReadValue,
         }
-    },
-    updateSave: function() {
+    }
+
+    updateSave = () => {
         while (updated_niceties_spinlock) {}
         let addString;
         if (this.props.data.stints.length > 0) {
@@ -81,74 +83,49 @@ const Person = React.createClass({
         }
         store.set("saved", false);
         this.props.saveReady();
-    },
-    textareaChange: function(event) {
+    }
+
+    textareaChange = (event) => {
         this.setState({textValue: event.target.value});
         store.set("nicety-" + this.props.data.id, event.target.value);
         this.updateSave();
-    },
-    anonymousChange: function(event) {
+    }
+
+    anonymousChange = (event) => {
         this.setState({anonValue: event.target.checked});
         store.set("anonymous-" + this.props.data.id, event.target.checked);
         this.updateSave();
-    },
-    noReadChange: function(event) {
+    }
+
+    noReadChange = (event) => {
         this.setState({noReadValue: event.target.checked});
         store.set("no_read-" + this.props.data.id, event.target.checked);
         this.updateSave();
-    },
+    }
 
-    // TODO: button for each person for anonymous option
-
-    // hold a callback in the parent and pass it to the child as a prop that gets called in onChange
-    // this callback would have the parent update its own min/max rows and pass this to all children
-    // but it seems like for this to work you need child.state.height (to figureo ut the new min rows)
-
-    render: function() {
-        let anonymousRender;
-        if (this.state.anonValue === true) {
-            anonymousRender = (
-                <Checkbox checked onChange={this.anonymousChange}>
-                    Submit Anonymously
-                </Checkbox>
-            );
-        } else if (this.state.anonValue === false) {
-            anonymousRender = (
-                <Checkbox onChange={this.anonymousChange}>
-                    Submit Anonymously
-                </Checkbox>
-            );
-        }
-
-        let noReadRender;
-        if (this.state.noReadValue === true) {
-            noReadRender = (
-                <Checkbox checked onChange={this.noReadChange}>
-                    Don't Read At Ceremony
-                </Checkbox>
-            );
-        } else if (this.state.noReadValue === false) {
-            noReadRender = (
-                <Checkbox onChange={this.noReadChange}>
-                    Don't Read At Ceremony
-                </Checkbox>
-            );
-        }
+    render() {
         return (
             <div className="person">
-                <Image responsive={true} src={this.props.data.avatar_url} circle={true} />
+                <Image responsive={true} src={this.props.data.avatar_url} circle={true}/>
                 <h3>{this.props.data.name}</h3>
-            <textarea
-                defaultValue={this.state.textValue}
-                onChange={this.textareaChange}
-                rows="6"
-                placeholder={this.props.data.placeholder}
-            />
-            {anonymousRender}
-            {noReadRender}
+                <textarea
+                    defaultValue={this.state.textValue}
+                    onChange={this.textareaChange}
+                    rows="6"
+                    placeholder={this.props.data.placeholder}/>
+                <Checkbox
+                    checked={this.state.anonValue === true}
+                    onChange={this.anonymousChange}>
+                    Submit Anonymously
+                </Checkbox>
+                <Checkbox
+                    checked={this.state.noReadValue === true}
+                    onChange={this.noReadChange}>
+                    Don't Read At Ceremony
+                </Checkbox>
             </div>
         );
     }
-});
+}
 
 export default Person;
