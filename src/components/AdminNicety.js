c6449ef1cf16d67d25775bb71096a70c12ee3d15
Christalee Bieber <cbieber@alum.mit.edu>
2021-11-07 13:13:07 -0500

    migration of AdminNicety to a functional component

--- src/components/AdminNicety.js
+++ src/components/AdminNicety.js
@@ -1,90 +1,33 @@
 import React from 'react';
-import {Checkbox} from 'react-bootstrap';
 
-import SaveButton from './SaveButton';
-
-class AdminNicety extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        text: this.props.nicety.text,
-        noSave: true,
-        reviewedValue: this.props.nicety.reviewed,
-    }
-  }
-
-    reviewedChange = (event) => {
-        this.setState({reviewedValue: event.target.checked, noSave: false,});
+const AdminNicety = (props) => {
+    let nicetyName;
+    if ('name' in props.nicety) {
+        nicetyName = props.nicety.name;
+    } else {
+        nicetyName = 'Anonymous';
     }
 
-    saveNicety = () => {
-        const data = {
-            text: this.state.text,
-            author_id: this.props.nicety.author_id,
-            end_date: this.props.nicety.end_date,
-            target_id: this.props.target_id,
-            faculty_reviewed: this.state.reviewedValue,
-        }
-        fetch(this.props.admin_edit_api, {
-          method: 'POST',
-          headers: {
-            'Content-Type': "application/json",
-          },
-          cache: 'no-cache',
-          body: JSON.stringify(data),
-        })
-        .then(() => this.setState({noSave: true}))
-        .catch((err) => console.log(err))
+    let noRead = null;
+    if (props.nicety.no_read === true) {
+        noRead = "Don't Read At Ceremony, Please";
     }
 
-    textareaChange = (event) => {
-        this.setState({text: event.target.value, noSave: false,});
+    let nicetyReturn = null;
+    if (props.nicety.text !== '' && props.nicety.text !== null) {
+        const textStyle = {
+            width: '75%',
+            whiteSpace: 'pre-wrap'
+        }
+        nicetyReturn = (
+            <div>
+                <h4>From {nicetyName}</h4>
+                <h5>{noRead}</h5>
+                <p style={textStyle}>{props.nicety.text}</p>
+            </div>
+        );
     }
-
-    render() {
-        let reviewedRender;
-        if (this.state.reviewedValue === true) {
-            reviewedRender = (
-                <Checkbox checked="checked" onChange={this.reviewedChange}>
-                    Reviewed
-                </Checkbox>
-            );
-        } else if (this.state.reviewedValue === false) {
-            reviewedRender = (
-                <Checkbox onChange={this.reviewedChange}>
-                    Reviewed
-                </Checkbox>
-            );
-        }
-
-        let nicetyName;
-        if ('name' in this.props.nicety) {
-            nicetyName = this.props.nicety.name;
-        } else {
-            nicetyName = 'Anonymous';
-        }
-
-        let noRead = null;
-        if (this.props.nicety.no_read === true) {
-            noRead = "Don't Read At Ceremony, Please";
-        }
-
-        let nicetyReturn = null;
-        if (this.props.nicety.text !== '' && this.props.nicety.text !== null) {
-            const textStyle = {
-                width: '75%',
-                whiteSpace: 'pre-wrap'
-            }
-            nicetyReturn = (
-                <div>
-                    <h4>From {nicetyName}</h4>
-                    <h5>{noRead}</h5>
-                    <p style={textStyle}>{this.props.nicety.text}</p>
-                </div>
-            );
-        }
-        return nicetyReturn;
-    }
+    return nicetyReturn;
 }
 
 export default AdminNicety;
