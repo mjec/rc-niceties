3a9d1ae80f8b6680135c66e92860ef3b06d457be
Christalee Bieber <cbieber@alum.mit.edu>
2022-01-19 18:38:28 -0500

    updated People/Person to use hooks

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,131 +1,128 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Checkbox, Image} from 'react-bootstrap';
 import store from 'store2';
 
 import {updated_niceties_spinlock} from "./People";
 
-class Person extends React.Component {
-    constructor(props) {
-        super(props)
-        let textValue = '';
-        let anonValue = false;
-        let noReadValue = false;
-        let dataPerson;
-        let foundPerson = false;
-        for (var i = 0; i < props.fromMe.length; i++) {
-            if (props.fromMe[i].target_id === props.data.id) {
-                dataPerson = props.fromMe[i];
-                foundPerson = true;
-                break;
-            }
-        }
-        let dateUpdated;
-        if (foundPerson) {
-            if (dataPerson.date_updated === null) {
-                dateUpdated = '';
-            } else {
-                dateUpdated = dataPerson.date_updated;
-            }
-        }
-        if (foundPerson && store.get("date_updated-" + props.data.id) === null) {
-            if (dataPerson.text !== '' && dataPerson.text !== null) {
-                store.set("nicety-" + props.data.id, dataPerson.text);
-                textValue = dataPerson.text;
-            } else {
-                textValue = '';
-            }
-            store.set("anonymous-" + props.data.id, dataPerson.anonymous);
-            anonValue = dataPerson.anonymous;
-            store.set("no_read-" + props.data.id, dataPerson.no_read);
-            noReadValue = dataPerson.no_read;
-            store.set("date_updated-" + props.data.id, dateUpdated);
-        } else if (foundPerson && store.get("date_updated-" + props.data.id) !== dateUpdated) {
-            if (dataPerson.text !== '' && dataPerson.text !== null) {
-                store.set("nicety-" + props.data.id, dataPerson.text);
-                textValue = dataPerson.text;
-            } else {
-                textValue = '';
-            }
-            store.set("anonymous-" + props.data.id, dataPerson.anonymous);
-            anonValue = dataPerson.anonymous;
-            store.set("no_read-" + props.data.id, dataPerson.no_read);
-            noReadValue = dataPerson.no_read;
-            store.set("date_updated-" + props.data.id, dateUpdated);
-        } else {
-            if (store.get("nicety-" + props.data.id) !== null) {
-                textValue = store.get("nicety-" + props.data.id);
-            }
-            if (store.get("anonymous-" + props.data.id) !== null) {
-                anonValue = store.get("anonymous-" + props.data.id);
-            }
-            if (store.get("no_read-" + props.data.id) !== null) {
-                noReadValue = store.get("no_read-" + props.data.id);
-            }
-        }
+const Person = (props) => {
 
-        this.state = {
-            textValue: textValue,
-            anonValue: anonValue,
-            noReadValue: noReadValue,
-        }
-    }
+    const [textValue, setTextValue] = useState("");
+    const [anonValue, setAnonValue] = useState(false);
+    const [noReadValue, setNoReadValue] = useState(false);
 
-    updateSave = () => {
+    const updateState = () => {
+      let dataPerson;
+      let foundPerson = false;
+      for (var i = 0; i < props.fromMe.length; i++) {
+          if (props.fromMe[i].target_id === props.data.id) {
+              dataPerson = props.fromMe[i];
+              foundPerson = true;
+              break;
+          }
+      }
+      let dateUpdated;
+      if (foundPerson) {
+          if (dataPerson.date_updated === null) {
+              dateUpdated = '';
+          } else {
+              dateUpdated = dataPerson.date_updated;
+          }
+      }
+      if (foundPerson && store.get("date_updated-" + props.data.id) === null) {
+          if (dataPerson.text !== '' && dataPerson.text !== null) {
+              store.set("nicety-" + props.data.id, dataPerson.text);
+              setTextValue(dataPerson.text);
+          } else {
+              setTextValue('');
+          }
+          store.set("anonymous-" + props.data.id, dataPerson.anonymous);
+          setAnonValue(dataPerson.anonymous);
+          store.set("no_read-" + props.data.id, dataPerson.no_read);
+          setNoReadValue(dataPerson.no_read);
+          store.set("date_updated-" + props.data.id, dateUpdated);
+      } else if (foundPerson && store.get("date_updated-" + props.data.id) !== dateUpdated) {
+          if (dataPerson.text !== '' && dataPerson.text !== null) {
+              store.set("nicety-" + props.data.id, dataPerson.text);
+              setTextValue(dataPerson.text);
+          } else {
+              setTextValue('');
+          }
+          store.set("anonymous-" + props.data.id, dataPerson.anonymous);
+          setAnonValue(dataPerson.anonymous);
+          store.set("no_read-" + props.data.id, dataPerson.no_read);
+          setNoReadValue(dataPerson.no_read);
+          store.set("date_updated-" + props.data.id, dateUpdated);
+      } else {
+          if (store.get("nicety-" + props.data.id) !== null) {
+              setTextValue(store.get("nicety-" + props.data.id));
+          }
+          if (store.get("anonymous-" + props.data.id) !== null) {
+              setAnonValue(store.get("anonymous-" + props.data.id));
+          }
+          if (store.get("no_read-" + props.data.id) !== null) {
+              setNoReadValue(store.get("no_read-" + props.data.id));
+          }
+      }
+  }
+
+  useEffect(() => {
+    updateState()
+  }, [textValue, anonValue, noReadValue])
+
+    const updateSave = () => {
         while (updated_niceties_spinlock) {}
         let addString;
-        if (this.props.data.stints.length > 0) {
-            addString = this.props.data.id + "," + this.props.data.stints[this.props.data.stints.length - 1].end_date;
+        if (props.data.stints.length > 0) {
+            addString = props.data.id + "," + props.data.stints[props.data.stints.length - 1].end_date;
         } else {
-            addString = this.props.data.id + ",2016-11-03";
+            addString = props.data.id + ",2016-11-03";
         }
-        if (!(addString in this.props.updated_niceties)) {
-            this.props.updated_niceties.add(addString);
+        if (!(addString in props.updatedNiceties)) {
+            props.setUpdatedNiceties(new Set([...props.updatedNiceties, addString]));
         }
         store.set("saved", false);
-        this.props.saveReady();
+        props.saveReady();
     }
 
-    textareaChange = (event) => {
-        this.setState({textValue: event.target.value});
-        store.set("nicety-" + this.props.data.id, event.target.value);
-        this.updateSave();
+    const textareaChange = (event) => {
+        setTextValue(event.target.value);
+        store.set("nicety-" + props.data.id, event.target.value);
+        updateSave();
     }
 
-    anonymousChange = (event) => {
-        this.setState({anonValue: event.target.checked});
-        store.set("anonymous-" + this.props.data.id, event.target.checked);
-        this.updateSave();
+    const anonymousChange = (event) => {
+        setAnonValue(event.target.checked);
+        store.set("anonymous-" + props.data.id, event.target.checked);
+        updateSave();
     }
 
-    noReadChange = (event) => {
-        this.setState({noReadValue: event.target.checked});
-        store.set("no_read-" + this.props.data.id, event.target.checked);
-        this.updateSave();
+    const noReadChange = (event) => {
+        setNoReadValue(event.target.checked);
+        store.set("no_read-" + props.data.id, event.target.checked);
+        updateSave();
     }
 
-    render() {
-        return (
-            <div className="person">
-                <Image responsive={true} src={this.props.data.avatar_url} title={this.props.data.full_name} circle={true}/>
-                <h3>{this.props.data.name}</h3>
-                <textarea
-                    defaultValue={this.state.textValue}
-                    onChange={this.textareaChange}
-                    rows="6"
-                    placeholder={this.props.data.placeholder}/>
-                <Checkbox
-                    checked={this.state.anonValue === true}
-                    onChange={this.anonymousChange}>
-                    Submit Anonymously
-                </Checkbox>
-                <Checkbox
-                    checked={this.state.noReadValue === true}
-                    onChange={this.noReadChange}>
-                    Don't Read At Ceremony
-                </Checkbox>
-            </div>
-        );
-    }
-}
+    return (
+          <div className="person">
+              <Image responsive={true} src={props.data.avatar_url} title={props.data.full_name} circle={true}/>
+              <h3>{props.data.name}</h3>
+              <textarea
+                  defaultValue={textValue}
+                  onChange={textareaChange}
+                  rows="6"
+                  placeholder={props.data.placeholder}/>
+              <Checkbox
+                  checked={anonValue === true}
+                  onChange={anonymousChange}>
+                  Submit Anonymously
+              </Checkbox>
+              <Checkbox
+                  checked={noReadValue === true}
+                  onChange={noReadChange}>
+                  Don't Read At Ceremony
+              </Checkbox>
+          </div>
+      );
+  }
 
 export default Person;
