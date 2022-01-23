f5f1a64e34963f5a8488fe0df6637211819ec71d
Christalee Bieber <cbieber@alum.mit.edu>
2022-01-23 17:18:17 -0500

    convert App to use hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import {Nav, Navbar, NavDropdown, MenuItem,} from 'react-bootstrap';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import store from 'store2';
 
@@ -16,89 +16,92 @@ if (store.get("saved") === null) {
     store.set("saved", true);
 }
 
-class App extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        fromMe: [],
-        people: [],
-        niceties: [],
-        currentview: "write-niceties",
-        selfInfo: []
+const App = (props) => {
+  const [fromMe, setFromMe] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [niceties, setNiceties] = useState([]);
+  const [selfInfo, setSelfInfo] = useState([])
+  const [currentView, setCurrentView] = useState('write-niceties');
+
+  const loadData = (api, setData) => {
+    fetch(api, {
+      headers: {'Content-Type': "application/json"},
+      cache: 'no-cache'
+    })
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    loadData(props.people_api, setPeople);
+  }, []);
+
+  useEffect(() => {
+    loadData(props.from_me_api, setFromMe);
+  }, []);
+
+  useEffect(() => {
+    loadData(props.for_me_api, setNiceties);
+  }, []);
+
+  useEffect(() => {
+    loadData(props.self_api, setSelfInfo);
+  }, []);
+
+  const handleSelect = (eventKey) => {
+      setCurrentView(eventKey);
+  }
+
+  const selectComponent = (idx) => {
+      switch (idx) {
+          case "write-niceties":
+              $('.dropdown-toggle').text('Write Niceties');
+              $('.dropdown-toggle').append('<span class="caret"></span>');
+              return <People
+                  people={people}
+                  fromMe={fromMe}
+                  save_nicety_api={props.save_nicety_api}/>
+          case "view-niceties":
+              $('.dropdown-toggle').text('Niceties About You');
+              $('.dropdown-toggle').append('<span class="caret"></span>');
+              return <NicetyDisplay niceties={niceties}/>
+          case "admin":
+              $('.dropdown-toggle').text('Admin');
+              $('.dropdown-toggle').append('<span class="caret"></span>');
+              return <Admin admin_edit_api={props.admin_edit_api}/>
+          default:
       }
-    }
+  }
 
-    loadData = (api, dataVar) => {
-      fetch(api, {
-        headers: {'Content-Type': "application/json"},
-        cache: 'no-cache'
-      })
-      .then(response => response.json())
-      .then(data => this.setState({[dataVar]: data}))
-      .catch(err => console.log(err))
-    }
+  let selectedComponent = selectComponent(currentView);
+  let adminMenu = null;
+  if (selfInfo.admin) {
+      adminMenu = (<MenuItem eventKey="admin">Admin</MenuItem>);
+  }
 
-    componentDidMount = () => {
-      this.loadData(this.props.people_api, "people")
-      this.loadData(this.props.from_me_api, "fromMe")
-      this.loadData(this.props.for_me_api, "niceties")
-      this.loadData(this.props.self_api, "selfInfo")
-    }
-
-    handleSelect = (eventKey) => {
-        this.setState({currentview: eventKey});
-    }
-
-    selectComponent = (idx) => {
-        switch (idx) {
-            case "write-niceties":
-                $('.dropdown-toggle').text('Write Niceties');
-                $('.dropdown-toggle').append('<span class="caret"></span>');
-                return <People
-                    people={this.state.people}
-                    fromMe={this.state.fromMe}
-                    save_nicety_api={this.props.save_nicety_api}/>
-            case "view-niceties":
-                $('.dropdown-toggle').text('Niceties About You');
-                $('.dropdown-toggle').append('<span class="caret"></span>');
-                return <NicetyDisplay niceties={this.state.niceties}/>
-            case "admin":
-                $('.dropdown-toggle').text('Admin');
-                $('.dropdown-toggle').append('<span class="caret"></span>');
-                return <Admin admin_edit_api={this.props.admin_edit_api}/>
-            default:
-        }
-    }
-
-    render() {
-        let selectedComponent = this.selectComponent(this.state.currentview);
-        let adminMenu = null;
-        if (this.state.selfInfo.admin === true) {
-            adminMenu = (<MenuItem eventKey="admin">Admin</MenuItem>);
-        }
-        return (
-            <div className="App">
-                <Navbar fixedTop id="main_nav">
-                    <div id="title">
-                        recurse<br/>
-                        nice-<br/>
-                        ties
-                    </div>
-                    <img id="octotie" src={octotie} alt="an octopus wearing a nice tie" height="153"/>
-                    <Nav activeKey={this.state.currentview} onSelect={this.handleSelect}>
-                        <NavDropdown>
-                            <MenuItem eventKey="write-niceties">Write Niceties</MenuItem>
-                            <MenuItem eventKey="view-niceties">Niceties About You</MenuItem>
-                            {adminMenu}
-                        </NavDropdown>
-                    </Nav>
-                </Navbar>
-                <div id="component_frame">
-                    {selectedComponent}
-                </div>
-            </div>
-        );
-    }
+  return (
+      <div className="App">
+          <Navbar fixedTop id="main_nav">
+              <div id="title">
+                  recurse<br/>
+                  nice-<br/>
+                  ties
+              </div>
+              <img id="octotie" src={octotie} alt="an octopus wearing a nice tie" height="153"/>
+              <Nav activeKey={currentView} onSelect={handleSelect}>
+                  <NavDropdown>
+                      <MenuItem eventKey="write-niceties">Write Niceties</MenuItem>
+                      <MenuItem eventKey="view-niceties">Niceties About You</MenuItem>
+                      {adminMenu}
+                  </NavDropdown>
+              </Nav>
+          </Navbar>
+          <div id="component_frame">
+              {selectedComponent}
+          </div>
+      </div>
+  );
 }
 
 export default App;
